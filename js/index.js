8c2fe65b58b4207aee126e1dd86d71a85f969d8e
ada_ip <ada.iglesias.pasamontes@gmail.com>
2022-06-21 20:47:34 +0200

    Add functions to check a player's selection

--- js/index.js
+++ js/index.js
@@ -12,3 +12,30 @@ function computerPlay(){
             return "Scissors";
     }
 }
+
+// This function capitalizes a player's selection
+function capitalizeSelection(selection) {
+    let selectionCapitalized = selection.charAt(0).toUpperCase();
+    for (i = 1; i < selection.length; i++) {
+        selectionCapitalized += selection.charAt(i).toLowerCase();
+    }
+    return selectionCapitalized;
+}
+
+// This function checks if the player's selection is a valid selection or if they have introduced a string not included in the options of the game instead
+function checkSelection(selection) {
+    let validSelection = false;
+    // If the string introduced by the player is not empty it is compared to all the valid options
+    if(!isEmpty(selection)) {
+        let selectionCapitalized = capitalizeSelection(selection);
+        if(selectionCapitalized === "Rock" || selectionCapitalized === "Paper" || selectionCapitalized === "Scissors"){
+            validSelection = true;
+        }
+    }
+    return validSelection;
+}
+
+// This function checks if a string is an empty string
+function isEmpty(string) {
+    return string.length === 0;
+}
